0291b7603cd550bcb1d4716150a6708a4eb255bd
Anish Khale <anish_khale@hotmail.com>
2013-02-23 16:07:56 -0500

    Minor modifications to Login class

---
+++ HardenedPassword/src/calculator/Login.java
@@ -0,0 +1,105 @@
+package calculator;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+public class Login 
+{
+	static String QA[][] = new String[5][4];
+
+	public static void main(String[] args) throws IOException, InvalidKeyException, NoSuchAlgorithmException 
+	{
+		int x, y, qa1, qa2;
+		int i;
+		String userName = new String();
+		String pwd = new String();
+		Scanner in = new Scanner(System.in);
+		{
+		QA[0][3] = "How far away from the college are you (in miles)?";
+		QA[1][3] = "How many Jelly Beans can fit in a 100 cc jar?";
+		QA[2][3] = "How many hours do you wish to be logged in?";
+		QA[3][3] = "What is your weight (in pounds)?";
+		QA[4][3] = "How deep do you think is the Pacific Ocean (in miles)?";
+		}
+		
+		Formulae f = new Formulae();
+		
+		System.out.println("Menu:");
+		System.out.println("1. Create account");
+		System.out.println("2. Login");
+		System.out.println("Enter your Choice:");
+		x = in.nextInt();
+		
+		System.out.println("User Name:");
+		userName = in.next();
+		System.out.println("Password:");
+		pwd = in.next();
+		
+		if(x==1)
+		{
+			for (i = 0; i < 5; i++)
+			{
+				System.out.println(QA[i][3]);
+				QA[i][1] = in.next();
+				QA[i][2] = QA[i][1];
+				qa1 = Integer.valueOf(QA[i][1]);
+				qa2 = Integer.valueOf(QA[i][2]);
+				if(qa1 > qa2)
+					QA[i][0] = "B";
+				else
+					if(qa1 < qa2)
+						QA[i][0] = "A";
+					else
+						if(qa1 == qa2)
+							QA[i][0] = "AB";
+			}
+			f.genPrime();
+			f.setHpwd();
+			f.calcPolynomial();
+			f.setInstTab();
+			//set history file
+			//encrypt
+			f.test();
+		}
+		else
+			if(x == 2)
+			{
+			System.out.println("Choose to answer some security questions:\n 1. YES,\t2. NO");
+			y = in.nextInt();
+			qa1= qa2 = 0;
+			switch(y)
+			  {
+			  case 1:	for (i = 0; i < 5; i++)
+				  		{
+				  			System.out.println(QA[i][3]);
+				  			QA[i][1] = in.next();
+				  			qa1 = Integer.valueOf(QA[i][1]);
+				  			qa2 = Integer.valueOf(QA[i][2]);
+				  			if(qa1 > qa2)
+				  				QA[i][0] = "B";
+				  			else
+				  				if(qa1 < qa2)
+				  					QA[i][0] = "A";
+				  				else
+				  					if(qa1 == qa2)
+				  						QA[i][0] = "AB";
+				  		}
+				  		break;
+				  		
+			  case 2:	for (i = 0; i < 5; i++)
+							QA[i][0] = "AB";
+				  		break;
+				  		
+			  default:	break;
+			  }
+					// XY calculation from instruction table using the Alpha or Beta values 
+								//we get from QA
+					// Hpwd calculation using the XY values
+					// decrypt and display and update
+			}
+		in.close();
+	}
+	
+}
